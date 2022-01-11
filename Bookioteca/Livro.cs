ac47d104a6da3183eeab1b77b18c1fe7db2d2395
Felipe Pereira de Sousa Barbosa <felipaohb2009@hotmail.com>
2022-01-11 11:48:52 -0300

    Add project files.

---
+++ Bookioteca/Livro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookioteca
+{
+    internal class Livro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Volume { get; set; }  
+        public string Observação { get; set; }
+        public string Emprestimo { get; set; }
+        public string DataDeEmprestimo { get; set; }
+    }
+}
+//autor, volume, observação, emprestimo, data do emprestimo.
---
+++ Bookioteca/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bookioteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Livro> essesLivros = new List<Livro>();
+
+            int i = 0;
+            while (true)
+            {
+                i++;
+                Livro _livro = new Livro();
+                Console.Write("Informe o título do " + i + " livro: ");
+                _livro.Titulo = Convert.ToString(Console.ReadLine());
+                if (_livro.Titulo == string.Empty)
+
+                {
+                    break;
+                }
+                Console.Write("Informe o autor do  " + i + " livro: ");
+                _livro.Autor = Convert.ToString(Console.ReadLine());
+                if (_livro.Autor == string.Empty)
+
+                {
+                    break;
+                }
+                Console.Write("Informe o volume do " + i + " livro:  ");
+                _livro.Volume = Convert.ToString(Console.ReadLine());
+                if (_livro.Volume == string.Empty)
+
+                {
+                    break;
+                    //autor, volume, observação, emprestimo, data do emprestimo.
+                }
+                Console.Write("Informe se existe alguma observação a ser feita sobre o  " + i + " livro: ");
+                _livro.Observação = Convert.ToString(Console.ReadLine());
+                if (_livro.Observação == string.Empty)
+
+                {
+                    break;
+                }
+                Console.Write("Informe se houve empréstimo do " + i + " livro: ");
+                _livro.Emprestimo = Convert.ToString(Console.ReadLine());
+                if (_livro.Emprestimo == string.Empty)
+
+                {
+                    break;
+                }
+                Console.Write("Informe a data do empréstimo do  " + i + " livro: ");
+                _livro.DataDeEmprestimo = Convert.ToString(Console.ReadLine());
+                if (_livro.DataDeEmprestimo == string.Empty)
+
+                {
+                    break;
+                    //autor, volume, observação, emprestimo, data do emprestimo.
+                }
+
+                essesLivros.Add(_livro);
+            }
+            foreach (Livro _livro in essesLivros.OrderBy(l => l.Titulo))
+            {
+                Console.WriteLine(_livro.Titulo);
+                Console.WriteLine(_livro.Autor);
+                Console.WriteLine(_livro.Volume);
+                Console.WriteLine(_livro.Observação);
+                Console.WriteLine(_livro.Emprestimo);
+                Console.WriteLine(_livro.DataDeEmprestimo);
+            }
+            Console.ReadKey();
+        }
+    }
+}
